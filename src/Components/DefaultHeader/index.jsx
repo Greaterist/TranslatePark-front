8959a31a4c71b75e196e253932414034851bca74
Ivan Taktaev <32547753+Greaterist@users.noreply.github.com>
2023-09-11 11:54:05 +0000

    search bar autocomplete

--- src/Components/DefaultHeader/index.jsx
+++ src/Components/DefaultHeader/index.jsx
@@ -1,19 +1,66 @@
-import { Button, Menu } from "antd";
+import { AutoComplete, Button, Input, Menu } from "antd";
 import Search from "antd/es/input/Search";
 import { Header } from "antd/es/layout/layout";
 import styles from "./index.module.scss";
-import { Link } from "react-router-dom";
+import { Link, redirect } from "react-router-dom";
+import { useState } from "react";
+
+const getRandomInt = (max, min = 0) => Math.floor(Math.random() * (max - min + 1)) + min;
+const searchResult = (query) =>
+  new Array(getRandomInt(5))
+    .join('.')
+    .split('.')
+    .map((_, idx) => {
+      const category = `${query}${idx}`;
+      return {
+        value: category,
+        label: (
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              color: "red",
+            }}
+          >
+            <span>
+              Found {query} on{' '}
+              <a
+                href={`https://s.taobao.com/search?q=${query}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {category}
+              </a>
+            </span>
+            <span>{getRandomInt(200, 100)} results</span>
+          </div>
+        ),
+      };
+    });
+
 
 const DefaultHeader = () => {
-
-    const onSearch = () =>{
-
-    }
+    const [options, setOptions] = useState([]);
+    const handleSearch = (value) => {
+      setOptions(value ? searchResult(value) : []); //TODO change searchResult to backend request 
+    };
+    const onSelect = (value) => {
+      console.log('onSelect', value);
+    };
 
     return (
         <Header style={{ display: 'flex', alignItems: 'center' }}>
             <Link to="/" className={styles.icon}> <img src="/icon.png" alt="" className={styles.icon}/> </Link>
-            <Search placeholder="input search text" onSearch={onSearch} style={{ width: 500 }} />
+            <AutoComplete
+            style={{
+              width: 300,
+            }}
+            options={options}
+            onSelect={onSelect}
+            onSearch={handleSearch}
+            >
+                <Input.Search size="large" placeholder="input here" enterButton />
+            </AutoComplete>
             <Link to="/profile/" className={styles.nav}> <Button>Profile</Button></Link>
         </Header>
     )
