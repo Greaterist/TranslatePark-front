5607061a271761f900a8b8a4d7e9e5b6f04e5e1f
Ivan <taktaef@yandex.ru>
2023-10-18 13:43:53 +0300

    auth page added

---
+++ src/Pages/AuthorizationPage/index.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import Defaultlayout from "../../Layouts/Default";
+import { Button, Form, Input } from "antd";
+import { PasswordInput } from "antd-password-input-strength";
+
+const AuthorizationPage = () => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const [level, setLevel] = useState(0);
+
+  const minLevel = 1;
+
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+
+  const validatePassword = (rule, value, callback) => {
+    if (value && value !== "Secret") {
+      callback("Error!");
+    } else {
+      callback();
+    }
+  };
+
+  return (
+    <Defaultlayout>
+      <p>AUTHORIZATION</p>
+      <Form
+        name="complex-form"
+        onFinish={onFinish}
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+      >
+        <Form.Item
+          name="email"
+          label="E-mail"
+          rules={[
+            {
+              type: "email",
+              message: "The input is not valid E-mail!",
+            },
+            {
+              required: true,
+              message: "Please input your E-mail!",
+            },
+          ]}
+        >
+          <Input placeholder="input Email" />
+        </Form.Item>
+        <Form.Item
+          rules={[
+            {
+              validator: async () => {
+                return level >= minLevel
+                  ? Promise.resolve()
+                  : Promise.reject(errorMessage);
+              },
+              message: "Password is too weak",
+            },
+          ]}
+          name="password"
+          label="Password"
+        >
+          <PasswordInput
+            placeholder="input password"
+            onLevelChange={setLevel}
+            visibilityToggle={{
+              visible: passwordVisible,
+              onVisibleChange: setPasswordVisible,
+            }}
+          />
+        </Form.Item>
+        <Form.Item label=" " colon={false}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </Defaultlayout>
+  );
+};
+
+export default AuthorizationPage;
--- src/Pages/RoutesList.jsx
+++ src/Pages/RoutesList.jsx
@@ -3,6 +3,7 @@ import HomePage from "./HomePage";
 import TranslationPage from "./TranslationPage";
 import ProfilePage from "./ProfilePage";
 import NewWordPage from "./NewWordPage";
+import AuthorizationPage from "./AuthorizationPage";
 
 const RoutesList = () => {
   return (
@@ -11,6 +12,7 @@ const RoutesList = () => {
       <Route path="/profile" element={<ProfilePage />} />
       <Route path="/translation/:id" element={<TranslationPage />} />
       <Route path="/newword/" element={<NewWordPage />} />
+      <Route path="/auth/" element={<AuthorizationPage />} />
     </Routes>
   );
 };
