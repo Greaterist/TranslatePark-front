c4309e42d77fd8b2c079a6ca28aa4d5fff882d85
Ivan Taktaev <32547753+Greaterist@users.noreply.github.com>
2023-10-24 14:37:59 +0000

    added redux auth

---
+++ src/redux/auth/index.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isAuth: localStorage.authenticated || false,
+  authToken: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setAuth(state, action) {
+      state.isAuth = action.payload;
+    },
+    setAuthToken(state, action) {
+      state.authToken = action.payload;
+    },
+  },
+});
+
+export const { setAuth, setAuthToken } = authSlice.actions;
+
+export default authSlice.reducer;
